9a901788a99f1fdb23fa492551f6bbd6bad2ed45
Sebastian Rivera Mira <101227585+SEBASZTIANRM@users.noreply.github.com>
2023-05-04 21:37:42 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ botonRegistrarse.addEventListener("click", function(){
     if( document.getElementById("campoNombre").value !== "" && document.getElementById("campoApellido").value && document.getElementById("campoIdentificacion").value !== ""
         && document.getElementById("campoCorreo").value !== "" && document.getElementById("campoCelular").value !== "" && document.getElementById("campoContraseña").value !== ""){
 
-         alertify.alert('Registro de usuarios', '¡Usuario registado correctamente!' + 
-        ' Se envio correo de confirmacion a: ' + document.getElementById("campoCorreo").value , function(){ alertify.success('¡Usuario registado correctamente!'); });
+         alertify.alert('Registro de usuarios','¡Usuario registado correctamente!' + 
+        'Se envio correo de confirmacion a: ' + document.getElementById("campoCorreo").value , function(){ alertify.success('¡Usuario registado correctamente!'); });
 
     }else{
 
