164ce778d08155df61b7d52779b4d0fb13c1a85a
Sebastian Rivera Mira <101227585+SEBASZTIANRM@users.noreply.github.com>
2023-05-07 17:44:51 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -16,12 +16,13 @@ botonRegistrarse.addEventListener("click", function(){
 })
 
 botonIniciarSesion.addEventListener("click",function(){
-    if(contador >=3){
-
-        alertify.alert('Inicio Sesión', '¡Usted supero el limite de intentos de iniciar sesión!', function(){ alertify.error('# intentos erroneos' + contador); });
     
+    if(contador >=3){
+        
+        alertify.alert('Inicio Sesión', '¡Usted supero el limite de intentos de iniciar sesión!', function(){ alertify.error('# intentos erroneos' + contador); });
        }else{
-
+           
         window.open("login.html")
+           
        }
 })
