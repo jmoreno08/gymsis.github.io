1ce1a0b4394d8046ab2a4476e75cf31b91684bf3
Sebastian Rivera Mira <101227585+SEBASZTIANRM@users.noreply.github.com>
2023-05-07 16:53:12 -0500

    Update index.js
    Utilizo el LocalStorage para almacenar y traer datos, en este caso un "contador" que le indica al script si el usuario ha superado el limite de "Inicio de sesión", si supera el limite bloqueara el boton de "Iniciar Sesión".
    Para eliminar los datos de sesion se debe borrar el cache del navegador xDDDD.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
+let contador = JSON.parse(localStorage.getItem('contador'));
+
 botonRegistrarse.addEventListener("click", function(){
-    
+
     if( document.getElementById("campoNombre").value !== "" && document.getElementById("campoApellido").value && document.getElementById("campoIdentificacion").value !== ""
         && document.getElementById("campoCorreo").value !== "" && document.getElementById("campoCelular").value !== "" && document.getElementById("campoContraseña").value !== ""){
 
@@ -11,5 +13,15 @@ botonRegistrarse.addEventListener("click", function(){
         alertify.alert('Registro de usuarios', '¡Se detectaron campos vacios!', function(){ alertify.error('¡Registro cancelado!'); });
     }
     
+})
 
+botonIniciarSesion.addEventListener("click",function(){
+    if(contador >=3){
+
+        alertify.alert('Inicio Sesión', '¡Usted supero el limite de intentos de iniciar sesión!', function(){ alertify.error('# intentos erroneos' + contador); });
+    
+       }else{
+
+        window.open("login.html")
+       }
 })
