e8263bd76446e8e9fea72cd8633ade64c038e5a6
Sebastian Rivera Mira <101227585+SEBASZTIANRM@users.noreply.github.com>
2023-05-07 21:38:11 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,26 @@ let contador = 0;
 
 botonRegistrarse.addEventListener("click", function(){
 
+   
+    
     if( document.getElementById("campoNombre").value !== "" && document.getElementById("campoApellido").value && document.getElementById("campoIdentificacion").value !== ""
         && document.getElementById("campoCorreo").value !== "" && document.getElementById("campoCelular").value !== "" && document.getElementById("campoContraseña").value !== ""){
 
          alertify.alert('Registro de usuarios','¡Usuario registado correctamente!' + 
         'Se envio correo de confirmacion a: ' + document.getElementById("campoCorreo").value , function(){ alertify.success('¡Usuario registado correctamente!'); });
+
     }else{
+
         alertify.alert('Registro de usuarios', '¡Se detectaron campos vacios!', function(){ alertify.error('¡Registro cancelado!'); });
     }
+    
+
 })
 
+
+
 botonLogin.addEventListener("click",function(){
+
     if(contador <= 2){
         if(document.getElementById("campoId").value !== "" &&
         document.getElementById("campoContraseña").value !== ""){
@@ -23,6 +32,10 @@ botonLogin.addEventListener("click",function(){
             alertify.alert('Iniciar Sesión','¡Usuario o contraseña incorrectos!', function(){ alertify.error('# Intentos erroneos: ' + contador); });
         }
     }else{
-        alertify.alert('¡Error!','Usted supero el limite de intentos al iniciar sesión', function(){ alertify.error('# Intentos erroneos: ' + contador); });
+        alertify.alert('¡Error!','Usted supero el limite de intentos al iniciar sesión. ¡Recargue la pagina!', function(){ alertify.error('# Intentos erroneos: ' + contador); });
     }
+    
 })
+
+
+
